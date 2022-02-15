658ac504d05167c95cbdc9474a72c86e3211ecb3
seoyoosuk <87297955+seoyoosuk@users.noreply.github.com>
2022-02-15 11:10:22 +0900

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -88,7 +88,7 @@
  
  var ebModal1 = document.getElementById("mark1Modal");
  var ebBtn1 = document.getElementById("mark1");
- var ebSpan1 = document.getElementsByClassName("exit")[0];
+ var ebSpan1 = document.getElementsByClassName("close")[0];
  
  ebBtn1.onclick = function () {
    ebModal1.style.display = "none";
